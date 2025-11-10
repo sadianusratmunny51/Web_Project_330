df04f5fa75dcc597c374607bd53e279b39016fb2
Hazerakteritu <hazerakteritu@gmail.com>
2025-11-10 22:58:59 +0600

    zz

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const db = require("./db");
+const db = require("./config/db");
 
 dotenv.config();
 
